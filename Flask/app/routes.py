df9e15adef323fcee46532d891dec187d94a6202
IbrahimK7 <91416572+IbrahimK7@users.noreply.github.com>
2025-10-29 18:40:04 +0100

    Added relevant endpoints to run.py and tested it.

--- Flask/app/routes.py
+++ Flask/app/routes.py
@@ -8,3 +8,28 @@ def register_routes(app):
     @app.route('/api/login')
     def login ():
         return jsonify({"message": "you have logged in!"})
+    
+    @app.route('/api/forgot-password')
+    def forgot_password():
+        return jsonify({"message": "password reset link sent!"})
+    
+    @app.route('/api/home')
+    def home():
+        return jsonify({"message": "Welcome to the home page!"})
+    
+    @app.route('/api/inbox')
+    def inbox():
+        return jsonify({"message": "Here is your inbox!"})
+    
+    @app.route('/api/host')
+    def host():
+        return jsonify({"message": "Host a party!"})
+    
+    @app.route('/api/search')
+    def search():
+        return jsonify({"message": "Search for a party!"})
+    
+    @app.route('/api/settings')
+    def settings():
+        return jsonify({"message": "User settings page!"})
+    
